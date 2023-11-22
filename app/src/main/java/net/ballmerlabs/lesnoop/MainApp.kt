9fd3f8d1b729605991463650e0df6b604fc9b714
fmeef <gnu3ra@riseup.net>
2023-11-22 17:56:18 -0600

    add global rxjava error handler

--- app/src/main/java/net/ballmerlabs/lesnoop/MainApp.kt
+++ app/src/main/java/net/ballmerlabs/lesnoop/MainApp.kt
@@ -1,7 +1,16 @@
 package net.ballmerlabs.lesnoop
 
 import android.app.Application
+import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
+import io.reactivex.rxjava3.plugins.RxJavaPlugins
 
 @HiltAndroidApp
-class MainApp : Application()
+class MainApp : Application() {
+    init {
+        RxJavaPlugins.setErrorHandler { err: Throwable ->
+            Log.e("global", "unhandled rxjava exception: $err")
+            err.printStackTrace()
+        }
+    }
+}
